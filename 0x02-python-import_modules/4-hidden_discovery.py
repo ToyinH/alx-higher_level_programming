87933b63b8b628bbd67c116cc1fa86920ddc35a6
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-11 16:48:01 +0000

    a program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def main():
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if not name[0:2] == "__":
+            print(name)
+
+if __name__ == "__main__":
+    main()
+
