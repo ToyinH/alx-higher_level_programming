80ca640a83005a1c1dee871aa86bd74ad74281ca
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-28 12:37:29 +0000

    a function that prints x elements of a lis handling exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
                 print("{}".format(element), end="")
             count += 1
         print()
-        return count
+        return x
     except IndexError:
-        return count
-
+        return x
