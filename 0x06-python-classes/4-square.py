344bad16bc1c03c827453f0a0fff4d234be2911c
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-30 02:43:34 +0000

    a class Square that defines a square by: (based on 3-square.py). Get and set private instance size

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+
     """This is the class Square."""
     def __init__(self, size=0):
         """this is the constructor method for the class."""
@@ -12,10 +13,21 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    def area(self):
+        """
+        Calculat the area of the square.
+        Return the gotten area.
+        """
+        return self.__size ** 2
+
+
+    @property
     def size(self):
+
         """Method to retrieve private object attribute, size."""
         return self.__size
 
+    @size.setter
     def size(self, value):
         """Method that set the size with value."""
         if not isinstance(value, int):
@@ -23,11 +35,3 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """
-        Calculate the area of the square.
-        Return the area gotten.
-        method that returns the current square area.
-        """
-        return self.__size ** 2
