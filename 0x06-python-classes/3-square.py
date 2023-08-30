1189c3883e15749023bebfdf2a9b1da28f91bf02
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-30 02:09:40 +0000

    a class Square that defines a square by: (based on 2-square.py) and returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Creating a class Square."""
+
+
+class Square:
+    """This is the class Square."""
+    def __init__(self, size=0):
+        """this is the constructor method for the class."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+        Return the area gotten.
+        method that returns the current square area.
+        """
+        return self.__size ** 2
