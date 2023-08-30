6b4a6898a78fa09f3da30b6e7752c70cadc52ac5
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-30 01:37:27 +0000

    a class Square that defines a square with private instance attirbute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Creating a class Square."""
+
+class Square:
+    """This is class Square with private attributes Size."""
+   def  __init__(self, size):
+        """This is the constructor method."""
+        self.__size = size
