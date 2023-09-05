4c263bf9dd76533d30f0d0410941b4b4f7a9fd15
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-05 04:19:47 +0000

    repr

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
 
     def __repr__(self):
         """return a string representation of the rectangle"""
-        return "Rectangle({}, {}).format(self.__width, self.__height)"
+        return f"Rectangle({self.__width}, {self.__height})"
 
     @property
     def width(self):
