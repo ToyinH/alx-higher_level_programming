cd6d8b518a196e2245f9ba6233f6b3b362643326
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-10 13:49:28 +0000

    function that returns two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+module to add two integers
+"""
+
+
+def add_integer(a, b=98):
+    """function that returns the sum of two integers"""
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
