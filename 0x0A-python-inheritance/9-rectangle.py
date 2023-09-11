c0241362842d0ddd13bd93e78c28f8f44e027d39
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-11 14:39:49 +0000

    Rectangle... str method and area method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A class BaseGeometry."""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """instantiation method."""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        """The str method."""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.__width * self.__height
