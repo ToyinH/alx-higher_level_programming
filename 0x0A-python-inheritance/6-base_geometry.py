19887ff4adcf598f43b814589df340c0c4d220bc
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-11 12:28:44 +0000

    write a class BaseGeometry with a pubblic instance method raising exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    def area(self):
+        """finding the area"""
+        raise Exception("area() is not implemented")
