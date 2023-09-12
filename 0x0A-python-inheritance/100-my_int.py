8a2f9d711fb007cddb4be3514e85c9b03b479a91
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-12 02:04:34 +0000

    Write a class MyInt that inherits from int:

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class MyInt which inherits from int and reverse the operation
+of == and !="""
+
+
+class MyInt(int):
+    """The new class which inherit from int and switch the function of ==
+    and !="""
+    def __eq__(self, other):
+        """the equal method"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """the not equal method"""
+        return super().__eq__(other)
