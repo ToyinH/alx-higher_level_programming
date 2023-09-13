ad91c7da0fd089be20036ee48c6d2da02b81548f
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-13 11:02:02 +0000

    a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -11,11 +11,12 @@ def class_to_json(obj):
     """The function that return the dictionary description of obj. All
     attributes of the obj Class are serializable."""
     json_dict = {}
-    attributes = dir(obj)
+    attributes = vars(obj)
+    print(attributes)
     for attribute_name in attributes:
         if attribute_name.startswith("__"):
             continue
-        attribute_value = getattr(obj, attribute_name)
+        attribute_value = attributes[attribute_name]
         if isinstance(attribute_value, (list, dict, str, int, bool)):
             json_dict[attribute_name] = attribute_value
     return json_dict
