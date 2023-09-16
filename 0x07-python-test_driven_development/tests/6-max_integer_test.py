dc7ca5c7b4c96aad9b768a8b13f8f52aea4d4e45
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-16 09:27:34 +0000

    unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""unittest file for the function max_integer"""
+
+
+from 6-max_integer import max_integer
+class testMaxInteger(unittest.TestCase):
+    """class to test max_integer"""
+
