3e6bfe42e181f00ac6c462e2074452d2a6a1d2ae
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-16 21:00:37 +0000

    updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -150,8 +150,15 @@ class Rectangle(Base):
             int: area is returned
         """
         return self.__width * self.__height
+    
+    def display(self):
+        """
+        Public method that prints to stdout the
+        Rectangle instance
+        """
+        for _ in range(self.__height):
+            print("#" * self.__width)
 
-
-
-
-        
+    def __str__(self):
+        """the __str__ method"""
+        return("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
