92c0c172ede0141e7e9705e2b09c567f6f724219
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-16 15:18:39 +0000

    unittest for base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,10 +9,10 @@ from base import Base
 def test_base(unittest.TestCase):
     """test case for class Base"""
     b1 = Base()
-    self.assertEqual(b1, 1)
+    self.assertEqual(b1.id, 1)
     b2 = Base()
-    self.assertEqual(b2, 2)
+    self.assertEqual(b2.id, 2)
     b3 = Base(10)
-    self.assertEqual(b3, 10)
+    self.assertEqual(b3.id, 10)
     b4 = Base()
-    self.assertEqual(b4, 3)
+    self.assertEqual(b4.id, 3)
