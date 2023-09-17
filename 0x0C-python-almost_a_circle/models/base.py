6e8937d997f3fc76622ade344b5af27d9d8f6dc1
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-17 10:00:24 +0000

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ of all other classes. The goal of it is to manage the id attribute
 in all future classes and to avoid duplicating the same code by extension,
 same bug.
 """
+import json
 
 
 class Base:
@@ -17,3 +18,20 @@ class Base:
         else:
             Base. __nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """static method that returns the JSON string
+        representation of list_dictionaries
+        
+        Args:
+            list_dictionaries(list): list of dictionaries
+            
+        Returns: returns the JSON string representation of
+        list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
