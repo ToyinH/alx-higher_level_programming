9ceaa87f438f832145f2b191424cbb9681c323be
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-17 09:11:05 +0000

    updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,3 +38,25 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        The update method for arguments and keyword arguments
+        for Square
+        
+        Args:
+            *args: arguments
+            **kwargs: keyword arguments
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
