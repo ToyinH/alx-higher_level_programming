5d52293d53e78e011f50f137dab5fedf584f79c6
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-17 05:37:51 +0000

    update display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -156,8 +156,10 @@ class Rectangle(Base):
         Public method that prints to stdout the
         Rectangle instance
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print((" " * self.__x) + ("#" * self.__width))
 
     def __str__(self):
         """the __str__ method"""
