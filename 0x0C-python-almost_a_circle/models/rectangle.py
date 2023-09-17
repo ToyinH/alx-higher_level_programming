211df4b531716ab8f85a13c8633cf788a194457b
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-17 09:23:55 +0000

    updated to return a dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -188,3 +188,18 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Public method that returns the dictionary representation
+        of Rectangle
+        
+        Returns: returns the dictionary representation of Rectangle
+        """
+        return {
+            "id": self.id,
+            "width": self.__width,
+            "height": self.__height,
+            "x": self.__x,
+            "y": self.__y
+        }
