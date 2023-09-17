ecb1ca18699c2e0910fa8f67e5fad39e6512dc88
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-17 06:08:13 +0000

    updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -164,3 +164,29 @@ class Rectangle(Base):
             self.id, self.__x, self.__y, self.__width, self.__height
             )
             )
+
+    def update(self, *args, **kwargs):
+        """
+        Method to update the class Rectangle
+        
+        Args:
+            *args: arguments. Assign an argument to each attribute
+            **kwargs: keyword arguments. Must be skipped if *args
+            exists and is not empty
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+
