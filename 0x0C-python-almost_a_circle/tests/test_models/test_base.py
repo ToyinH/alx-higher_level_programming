d3f886b26f87957997f73ffe174a2103a869b417
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-18 23:17:04 +0000

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -18,4 +18,33 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b3.id, 10)
         b4 = Base()
         self.assertEqual(b4.id, 3)
+
+    def test_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
+
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+        self.assertEqual(Base(12).id, 12)
+
+    def test_unique_id(self):
+        b1 = Base()
+        b2 = Base(10)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
+
+    def test_id_change(self):
+        b1 = Base()
+        b1.id = 13
+        self.assertEqual(b1.id, 13)
+
+
+
+    
         
