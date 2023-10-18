b726d6897651ccd657c518e0c1ec197999b6aa98
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-10-18 20:32:12 +0000

    script that lists all shows contained in the database hbtn_0d_tvshows.

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Select shows and their associated genre IDs (with NULL for shows without a genre)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
