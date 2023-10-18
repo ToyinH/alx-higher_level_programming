9922a9f0a0c5ac3267e4e4ad943f9e5cdd87017b
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-10-18 20:03:16 +0000

    script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create or modify user user_0d_2 with the password 'user_0d_2_pwd'
-CREATE USER IF NOT EXISTS user_0d_2@%localhost IDENTIFIED IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege to user_0d_2 on the hbtn_0d_2 database
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@%localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
