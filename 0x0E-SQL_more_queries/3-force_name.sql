e90c969ca4a98752d0d7b7bf9538db0a1170ac7f
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-10-18 20:09:37 +0000

    script that creates the table force_name on your MySQL server

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,6 @@
 -- Create the table 'force_name' if it doesn't exist
+
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
