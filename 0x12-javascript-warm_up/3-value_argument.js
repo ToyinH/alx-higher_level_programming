f19a5780b17c6b07575133dbbbf541e6386e9092
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-11-13 17:24:57 +0000

    print first arg

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const argCount = process.argv.length;
-if (argCount === 2) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
+const firstArg = process.argv[2];
+if (firstArg === undefined){
+  console.log('No argument')
+} else{
+  console.log(firstArg)
 }
