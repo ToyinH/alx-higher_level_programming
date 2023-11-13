98dac886173628e9f0686805dd890886bfcd0c12
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-11-13 17:20:04 +0000

    print argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const argCount = process.argv.length - 2;
-if (argCount === 0) {
+const argCount = process.argv.length;
+if (argCount === 2) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
