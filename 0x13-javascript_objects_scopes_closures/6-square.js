2b61baad2c36b68279db98e255cd3c2567781774
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-11-14 18:23:06 +0000

    square class with charprint func

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ class Square extends SquareP {
     if (c === undefined) {
       c = 'X';
     }
-    
+
     for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
