1b814c9913953294ac971051fdc262bcd6133b21
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-11-19 06:06:05 +0000

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect (
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
@@ -21,7 +21,3 @@ if __name__ == "__main__":
 
     cursor.close()
     conn.close()
-
-
-
-       
