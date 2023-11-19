329cb7a53c5d950feeeffab017a4a95abddb0248
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-11-19 06:03:42 +0000

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect (
-        host="Localhost",
+        host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
