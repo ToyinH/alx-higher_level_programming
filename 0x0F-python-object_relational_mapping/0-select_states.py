568792cc6e763b7e176dddfe6f731642de7ac6d1
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-11-19 06:04:31 +0000

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
         host="localhost",
         port=3306,
         user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        password=sys.argv[2],
+        database=sys.argv[3]
     )
 
     cursor = conn.cursor()
