6f5bd0a673c17b51c0027cc7cc9ef3434fcb64f3
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-11-30 14:36:10 +0000

    a function that finds a peak in a list of unsorted integers.

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,33 @@
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+
+    Args:
+    - list_of_integers: List of unsorted integers.
+
+    Returns:
+    - The peak element in the list.
+    """
+
+    # Get the length of the list
+    n = len(list_of_integers)
+
+    # Base case: if the list is empty, return None
+    if n == 0:
+        return None
+
+    # Binary search-like algorithm to find a peak
+    left, right = 0, n - 1
+    while left < right:
+        mid = (left + right) // 2
+
+        # Compare mid element with its neighbors
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            # Move towards the left side
+            right = mid
+        else:
+            # Move towards the right side
+            left = mid + 1
+
+    # The peak is found at index 'left'
+    return list_of_integers[left]
