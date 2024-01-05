57c791bf6e1ae5560af17431a5df1e3f516e2c95
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-01-05 14:35:24 +0000

    script that computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,51 @@
+#!/usr/bin/node
+/**
+ * script that computes the number of tasks completed by user id.
+ * The first argument is the
+ * API URL: https://jsonplaceholder.typicode.com/todos
+ * Only print users with completed task
+ *
+ */
+
+const request = require('request');
+
+const url = process.argv[2];
+
+// const url = 'https://jsonplaceholder.typicode.com/todos';
+
+const newDict = {};
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const list = JSON.parse(body);
+
+  const listSize = list.length;
+  // console.log(list);
+  // console.log(listSize);
+
+  let count = 0;
+  let id = 1;
+
+  for (let index = 0; index < listSize; index++) {
+    if (list[index].userId === id) {
+      if (list[index].completed === true) {
+        count++;
+      }
+    } else {
+      newDict[String(id)] = count;
+      if (list[index].completed === true) {
+        count = 1;
+      } else {
+        count = 0;
+      }
+      id++;
+    }
+  }
+  const lastKey = list[listSize - 1].userId;
+  newDict[String(lastKey)] = count;
+  console.log(newDict);
+});
