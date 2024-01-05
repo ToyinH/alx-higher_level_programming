df81d23028a639be43661a6bc4a110bc16d7ea95
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-01-05 05:03:21 +0000

    script to write to a file in js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+script to write a file */
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filepath, data, (err) => {
+  if (err) {
+    console.log(err);
+  }
+}
+);
