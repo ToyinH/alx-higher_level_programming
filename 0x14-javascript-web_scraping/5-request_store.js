87a94619b249fef929af7ed139c5fa3b8dddb88b
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-01-05 13:10:34 +0000

    a script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/**
+ * a script that gets the contents of a webpage and stores it in a file
+ * The first argument is the URL to request
+ * The second argument the file path to store the body response
+ * The file must be UTF-8 encoded
+ */
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filepath = process.argv[3];
+
+request.get(url, (err, response, body) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    // console.log(JSON.parse(body));
+    
+
+    // const data = JSON.parse(body);
+
+    fs.writeFile(filepath, body, (err) => {
+        if (err) {
+            console.log("error");
+            return;
+        }
+    }); 
+
+});
+
+
+
